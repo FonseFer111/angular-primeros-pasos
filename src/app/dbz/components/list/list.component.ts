cad2f8f42edc4cf4ea969f61bd6dff1aca528ff3
FernandoF <fernando.fonseca@smartdici.com>
2023-07-19 23:05:31 -0400

    Se sigue a parti de modulo nuevo componente character

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input() 
+  public characterList: Character[] = [{
+    id: uuid(),
+    name: 'Trunks',
+    power: 10
+  }]
+
+  deleteCharacterById( id?:string ):void{
+    if (!id) return;
+
+    this.onDelete.emit(id);
+
+  }
+
+
+}
